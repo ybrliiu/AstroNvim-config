5c4f2e0483350be8202ab2326c6b5ad6b57f70ec
ybrliiu <raian@reeshome.org>
2023-03-17 23:21:53 +0900

    perlnavigatorの設定

---
+++ init.lua
@@ -0,0 +1,30 @@
+return {
+    plugins = {
+        ["neo-tree"] = {
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                },
+            },
+        },
+    },
+    lsp = {
+        servers = {
+            'perlnavigator',
+        },
+        ["server-settings"] = {
+            perlnavigator = {
+                settings = {
+                    perlnavigator = {
+                        includePaths = {
+                            "$workspaceFolder/lib",
+                            "$workspaceFolder/local/lib/perl5",
+                            "$workspaceFolder/local/lib/perl5/x86_64-linux-gnu-thread-multi",
+                        },
+                        perlimportsTidyEnabled = true,
+                    },
+                },
+            },
+        },
+    },
+}
